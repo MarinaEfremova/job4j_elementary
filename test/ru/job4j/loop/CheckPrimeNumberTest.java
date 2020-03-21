31e7935b1d7c58f9edd416da0e8cb24668a2213d
Marina Efremova <efremovamarina2509@gmail.com>
2020-03-21 18:04:05 +0300

    5.5. Простое число.[#257967]

---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when5() {
+        boolean res = CheckPrimeNumber.check(5);
+        assertThat(res, is(true));
+    }
+
+    @Test
+    public void when4() {
+        boolean res = CheckPrimeNumber.check(4);
+        assertThat(res, is(false));
+    }
+
+    @Test
+    public void when1() {
+        boolean res = CheckPrimeNumber.check(1);
+        assertThat(res, is(false));
+    }
+}
