6b8f27fcc4bf95fb9b6146fb3a4d693d21dd583b
Marina Efremova <efremovamarina2509@gmail.com>
2020-03-22 11:58:42 +0300

    6.0.1. Объявление массива.[#257920]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "cat";
+        names[1] = "dog";
+        names[2] = "mouse";
+        names[3] = "duck";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
