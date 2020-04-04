a7119d77b267183be9edb8206c55e55ac10694f3
Marina Efremova <efremovamarina2509@gmail.com>
2020-04-04 21:21:22 +0300

    2. Перегрузка конструктора.[#258065]

---
+++ src/ru/job4j/array/tracker/Animal.java
@@ -0,0 +1,14 @@
+package ru.job4j.array.tracker;
+
+public class Animal {
+    private String name;
+
+    public Animal() {
+        super();
+        System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
